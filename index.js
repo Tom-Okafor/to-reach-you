3a684e97f80d93e1430aa4023bd2400db6e06dec
Tom Okafor <tmeka008@gmail.com>
2024-10-26 00:56:35 +0100

    Create Partials directory, necessary ejs files, a styles and scripts folder in the public directory

--- index.js
+++ index.js
@@ -3,8 +3,13 @@
 import express from "express";
 import multer from "multer";
 import ejs from "ejs";
+import path from "path";
 const APP = express();
 const PORT = 1510;
+
+APP.set("views", path.join(__dirname, "views/"));
+APP.set("view engine", "ejs");
+APP.use(express.static(path.join(__dirname, "public/")));
 
 APP.listen(PORT, err => {
     if (err) throw err;
