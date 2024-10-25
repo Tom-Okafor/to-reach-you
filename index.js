28db45b008c0719f8ef5e00537a545652552d5b8
Tom Okafor <tmeka008@gmail.com>
2024-10-26 00:36:35 +0100

    Import necessary modules and set up express.

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 /* NODE APPLICATION */
+
+import express from "express";
+import multer from "multer";
+import ejs from "ejs";
+const APP = express();
+const PORT = 1510;
+
+APP.listen(PORT, err => {
+    if (err) throw err;
+    console.log(`Server is currently listening at http://localhost:${PORT}`);
+});
