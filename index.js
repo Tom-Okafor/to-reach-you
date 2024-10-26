076aa3cc3c73028e25a74f18f02e2493c82a85ec
Tom Okafor <tmeka008@gmail.com>
2024-10-26 17:12:54 +0100

    Add static files and update path for views and static.

--- index.js
+++ index.js
@@ -3,13 +3,14 @@
 import express from "express";
 import multer from "multer";
 import ejs from "ejs";
-import path from "path";
+import { __dirname } from "path";
+import { fileURLToPath } from "url";
 const APP = express();
 const PORT = 1510;
 
-APP.set("views", "views/");
+APP.set("views", `${__dirname(fileURLToPath(import.meta.url))}/views/`);
 APP.set("view engine", "ejs");
-APP.use(express.static("public/"));
+APP.use(express.static(`${__dirname(fileURLToPath(import.meta.url))}/public/`));
 
 APP.get("/", (request, response) => {
     response.render("index.ejs");
