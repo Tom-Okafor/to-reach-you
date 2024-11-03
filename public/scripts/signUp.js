23a68fd6346132b10f602db04a3b0de20eb7e116
Tom Okafor <tmeka008@gmail.com>
2024-11-03 22:52:13 +0100

    Update SignUp form. Add firstName Validation to js.

--- public/scripts/signUp.js
+++ public/scripts/signUp.js
@@ -11,5 +11,23 @@
         const VALID_EMAIL = /^[\w.]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,4}$/;
         const VALID_PASSWORD =
             /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@=!?#&*"'√π$%`]).{8,}$/;
+        const FIRSTNAME = document.getElementById("firstname");
+        const LASTNAME = document.getElementById("lastname");
+        const EMAIL = document.getElementById("email");
+        const PASSWORD = document.getElementById("password");
+        const PASSWORD_CHECK = document.getElementById("passwordCheck");
+        if (!VALID_NAME.test(FIRSTNAME.value)) {
+          FIRSTNAME.focus()
+            const CLASSNAME = FIRSTNAME.getAttribute("id");
+            showFormError(
+                CLASSNAME,
+                "Please ensure you have inputed a valid firstname"
+            );
+            return false
+        }
+    }
+
+    function showFormError(className, message) {
+        document.querySelector(`.${className}`).innerText = message;
     }
 })();
