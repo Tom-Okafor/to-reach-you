5399c96b97c28a417955445d634fdcaa2869b916
Tom Okafor <tmeka008@gmail.com>
2024-11-04 01:13:04 +0100

    Refactor Validation code. Update SignUp page ejs file.

--- public/scripts/signUp.js
+++ public/scripts/signUp.js
@@ -1,5 +1,6 @@
 (() => {
     // script for Sign Up Page
+
     document.querySelector("form").addEventListener("submit", event => {
         if (!validateForm()) {
             event.preventDefault();
@@ -16,18 +17,57 @@
         const EMAIL = document.getElementById("email");
         const PASSWORD = document.getElementById("password");
         const PASSWORD_CHECK = document.getElementById("passwordCheck");
-        if (!VALID_NAME.test(FIRSTNAME.value)) {
-          FIRSTNAME.focus()
-            const CLASSNAME = FIRSTNAME.getAttribute("id");
-            showFormError(
-                CLASSNAME,
-                "Please ensure you have inputed a valid firstname"
-            );
-            return false
+
+        if (
+            !checkValue(
+                FIRSTNAME,
+                VALID_NAME,
+                "Please, input a valid firstname"
+            )
+        ) {
+            return false;
         }
+
+        if (
+            !checkValue(LASTNAME, VALID_NAME, "Please, input a valid lastname")
+        ) {
+            return false;
+        }
+
+        if (!checkValue(EMAIL, VALID_EMAIL, "Please, input a valid email")) {
+            return false;
+        }
+
+        if (
+            !checkValue(
+                PASSWORD,
+                VALID_PASSWORD,
+                "Please, input a valid password"
+            )
+        ) {
+            return false;
+        }
+
+        if (PASSWORD.value !== PASSWORD_CHECK.value) {
+            const CLASSNAME = PASSWORD_CHECK.getAttribute("id");
+            showFormError(CLASSNAME, "Your passwords do not match");
+            return false;
+        }
+        return true
     }
 
     function showFormError(className, message) {
         document.querySelector(`.${className}`).innerText = message;
+    }
+
+    function checkValue(input, regularExp, message) {
+        if (!regularExp.test(input.value)) {
+            input.focus();
+            const CLASSNAME = input.getAttribute("id");
+            showFormError(CLASSNAME, message);
+            return false;
+        } else {
+            return true;
+        }
     }
 })();
