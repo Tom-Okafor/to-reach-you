3cac319081e2af6335a9271f0bb0b2ba486aa541
Tom Okafor <tmeka008@gmail.com>
2024-11-11 15:29:31 +0100

    More styles and Animation for home page.

--- public/scripts/homePage.js
+++ public/scripts/homePage.js
@@ -1,5 +1,5 @@
 (() => {
-   /* function createGrid() {
+    /* function createGrid() {
         const DEVICE_WIDTH = window.innerWidth;
         const DEVICE_HEIGHT = window.innerHeight;
 
@@ -17,4 +17,56 @@
         }
     }
     createGrid();*/
+
+    function addBackgroundText() {
+        const BG_TEXT_HOLDER = document.querySelector(".bgText");
+        const TEXT = "to reach you ";
+        let bulkText = "";
+        const HEIGHT = document.querySelector(".main").clientHeight;
+        for (let i = 1000; i > 0; i--) {
+            bulkText += TEXT;
+        }
+        BG_TEXT_HOLDER.innerText += bulkText;
+        BG_TEXT_HOLDER.style.height = HEIGHT + "px";
+        console.log(BG_TEXT_HOLDER.style.height);
+    }
+
+    function moveBall() {
+        const BALL = document.querySelector(".ball");
+        const CONTAINER = document.querySelector(".main");
+        CONTAINER.addEventListener("touchstart", event => {
+            const LEFT_TOUCH = event.touches[0].clientX;
+            const LEFT_POSITION = LEFT_TOUCH - CONTAINER.offsetLeft;
+            const TOP_TOUCH = event.touches[0].clientY;
+            const TOP_POSITION = TOP_TOUCH - CONTAINER.offsetTop;
+            const BALL_WIDTH = BALL.offsetWidth;
+            const BALL_HEIGHT = BALL.offsetHeight;
+            BALL.style.transform = `translate(${
+                LEFT_POSITION - BALL_WIDTH / 2
+            }px, ${TOP_POSITION - BALL_HEIGHT / 2}px)`;
+            if (event.touches[0].target.tagName === "FOOTER") {
+                document.querySelector("footer").style.color = "var(--color3)";
+            } else {
+                document.querySelector("footer").style.color = "var(--color2)";
+            }
+        });
+        CONTAINER.addEventListener("mousemove", event => {
+            const LEFT_TOUCH = event.touches[0].clientX;
+            const LEFT_POSITION = LEFT_TOUCH - CONTAINER.offsetLeft;
+            const TOP_TOUCH = event.touches[0].clientY;
+            const TOP_POSITION = TOP_TOUCH - CONTAINER.offsetTop;
+            const BALL_WIDTH = BALL.offsetWidth;
+            const BALL_HEIGHT = BALL.offsetHeight;
+            BALL.style.transform = `translate(${
+                LEFT_POSITION - BALL_WIDTH / 2
+            }px, ${TOP_POSITION - BALL_HEIGHT / 2}px)`;
+            if (event.touches[0].target.tagName === "FOOTER") {
+                document.querySelector("footer").style.color = "var(--color3)";
+            } else {
+                document.querySelector("footer").style.color = "var(--color2)";
+            }
+        });
+    }
+    addBackgroundText();
+    moveBall();
 })();
