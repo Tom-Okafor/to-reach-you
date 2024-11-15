5512e23603797d29ed5f0f75d1e01a770abacd8f
Tom Okafor <tmeka008@gmail.com>
2024-11-15 11:45:54 +0100

    Finish styles and functions for mousemove and touchmove event.

--- public/scripts/homePage.js
+++ public/scripts/homePage.js
@@ -34,6 +34,32 @@
     function moveBall() {
         const BALL = document.querySelector(".ball");
         const CONTAINER = document.querySelector(".main");
+        const ELEMENTS_TO_BLEND_WITH = document.querySelectorAll(".blend");
+        const DIMENSIONS = [];
+
+        ELEMENTS_TO_BLEND_WITH.forEach(element => {
+            DIMENSIONS.push({
+                leftEdge: element.getBoundingClientRect().x,
+                rightEdge:
+                    element.getBoundingClientRect().x + element.offsetWidth,
+                topEdge: element.getBoundingClientRect().y,
+                bottomEdge:
+                    element.getBoundingClientRect().y + element.offsetHeight
+            });
+        });
+        function isMouseOnBlendElement(
+            mousePositionX,
+            mousePositionY,
+            collection
+        ) {
+            return (
+                mousePositionX >= collection.leftEdge &&
+                mousePositionX <= collection.rightEdge &&
+                mousePositionY >= collection.topEdge &&
+                mousePositionY <= collection.bottomEdge
+            );
+        }
+
         CONTAINER.addEventListener("touchmove", event => {
             handleMovement(event.touches[0]);
         });
@@ -48,14 +74,25 @@
             const TOP_POSITION = TOP_TOUCH - CONTAINER.offsetTop;
             const BALL_WIDTH = BALL.offsetWidth;
             const BALL_HEIGHT = BALL.offsetHeight;
+            let touchIndex = 0;
+            DIMENSIONS.forEach((eachDimension, index) => {
+                if (
+                    isMouseOnBlendElement(LEFT_TOUCH, TOP_TOUCH, eachDimension)
+                ) {
+                    touchIndex++;
+                }
+            });
+            if (touchIndex) {
+                BALL.classList.add("ball-blend");
+                console.log("touching");
+                touchIndex = 0;
+            } else {
+                BALL.className = "ball";
+            }
+
             BALL.style.transform = `translate(${
                 LEFT_POSITION - BALL_WIDTH / 2
             }px, ${TOP_POSITION - BALL_HEIGHT / 2}px)`;
-            if (event.target.tagName === "FOOTER") {
-                document.querySelector("footer").style.color = "var(--color3)";
-            } else {
-                document.querySelector("footer").style.color = "var(--color2)";
-            }
         }
     }
     addBackgroundText();
