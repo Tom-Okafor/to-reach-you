be7e33e5f56589e89295134a324e4f73da5b3b77
Tom Okafor <tmeka008@gmail.com>
2024-11-18 19:08:54 +0100

    Create function that returns indexes for shuffled posts.

--- controllers/homePage.controller.js
+++ controllers/homePage.controller.js
@@ -13,6 +13,38 @@ async function getData() {
 }
 const DATA = await getData();
 
+const SHUFFLE_INDEX = function () {
+    const SHUFFLE_ARR = [];
+    const NUM_OF_KEYS = Object.keys(DATA).length;
+    const NUM_OF_CONTENT = 5;
+    const KEYS_ARRAY = createIndexArray(5, NUM_OF_KEYS);
+    const VALUES_ARRAY = createIndexArray(4, NUM_OF_CONTENT);
+    SHUFFLE_ARR.push(KEYS_ARRAY);
+    SHUFFLE_ARR.push(VALUES_ARRAY);
+    function createIndexArray(iterations, range) {
+        const RANDOM_ARRAY = [];
+        for (let i = 0; i < iterations; i++) {
+            let index = generateRandomNum(range);
+            addIndexToArray(index, RANDOM_ARRAY, range);
+        }
+        return RANDOM_ARRAY;
+    }
+    function addIndexToArray(index, collection, range) {
+        if (collection.indexOf(index) === -1) {
+            collection.push(index);
+        } else {
+            let newIndex = generateRandomNum(range);
+            addIndexToArray(newIndex, collection, range);
+        }
+    }
+
+    function generateRandomNum(range) {
+        return Math.floor(Math.random() * range);
+    }
+    return SHUFFLE_ARR;
+};
+console.log(SHUFFLE_INDEX())
+
 ROUTER.use(bodyParser.urlencoded({ extended: true }));
 ROUTER.post("/", (request, response) => {
     const SIGN_IN_INPUT = request.body;
@@ -26,11 +58,13 @@ ROUTER.post("/", (request, response) => {
             errorMessage: "Incorrect Password"
         });
     } else {
-        response.render("templates/homePage.ejs", { DATA });
+        response.render("templates/homePage.ejs", { pageUrl, DATA });
     }
 });
 
-ROUTER.get("/", (request, response) => {        response.render("templates/homePage.ejs", { DATA });
+ROUTER.get("/", (request, response) => {
+    pageUrl = request.baseUrl;
 
+    response.render("templates/homePage.ejs", { pageUrl, DATA });
 });
 export { ROUTER };
