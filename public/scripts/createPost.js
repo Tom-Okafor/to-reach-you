909dfff412ca295221d5076db381b1f2d5d0b1b0
Tom Okafor <tmeka008@gmail.com>
2024-12-11 22:36:14 +0100

    Validate create post form.

--- public/scripts/createPost.js
+++ public/scripts/createPost.js
@@ -4,10 +4,23 @@
     const TITLE_INPUT = document.getElementById("title");
     const CATEGORY_OPTION = document.getElementById("category");
     const CONTENT_INPUT = document.getElementById("content");
-    // if (TITLE_INPUT.value.length > 200) {
-    //   return;
-    // }
+    if (TITLE_INPUT.value.length > 200) {
+      console.log(TITLE_INPUT.value);
+      return false;
+    }
+    if (CATEGORY_OPTION.value === "none") {
+      console.log(CATEGORY_OPTION.value);
+      return false;
+    }
+    if (CONTENT_INPUT.value.length < 700) {
+      console.log(CONTENT_INPUT.value);
+      return false;
+    }
+    return true;
     console.log(TITLE_INPUT.parentElement.querySelector("p").innerText);
   };
-  VALIDATE_FORM();
+
+  FORM_ELEM.addEventListener("submit", () => {
+    console.log(VALIDATE_FORM());
+  });
 })();
